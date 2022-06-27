fdcbc5bd34b4301ba60808ad60e5251c74690601
abraham38 <abraham@mobil.aullox.com>
2022-06-27 00:47:14 -0500

    Se agrego la funcionalidad de busqueda

--- js/main.js
+++ js/main.js
@@ -53,3 +53,55 @@ function cerrarNavbar() {
     if (!$(this).parent().hasClass("dropdown"))
         $(".navbar-collapse").collapse("hide");
 }
+
+/* Codigo para el modal */
+let cerrar = document.getElementById("btnCerrar");
+let abrir = document.getElementById("btnAbrir");
+let modal = document.getElementById("divModal");
+let contenedorModal = document.getElementById("contenedorModal");
+let body = document.getElementById("body");
+
+abrir.style.cursor = "pointer";
+
+abrir.addEventListener("click", function() {
+    modal.style.opacity = "1";
+    modal.style.visibility = "visible";
+    body.classList.add("body-scroll");
+    contenedorModal.classList.toggle("modal-cerrar");
+});
+
+cerrar.addEventListener("click", function() {
+    contenedorModal.classList.toggle("modal-cerrar");
+    modal.style.opacity = "0";
+    modal.style.visibility = "hidden";
+    body.classList.toggle("body-scroll");
+});
+
+/* Funcion del filtrado de busqueda */
+
+let inputBuscador = document.getElementById("inputB");
+let cajaResultados = document.getElementById("cajaResultados");
+
+document.getElementById("inputB").addEventListener("keyup", buscador);
+
+function buscador() {
+    var filtro = inputBuscador.value.toUpperCase();
+    var li = cajaResultados.getElementsByTagName("li");
+
+    /* Se recorre los elementos */
+    for (i = 0; i < li.length; i++) {
+        var a = li[i].getElementsByTagName("a")[0];
+        var valorTexto = a.textContent || a.innerText;
+
+        if (valorTexto.toUpperCase().indexOf(filtro) > -1) {
+            li[i].style.display = "";
+            cajaResultados.style.display = "block";
+
+            if (inputBuscador.value === "") {
+                cajaResultados.style.display = "none";
+            }
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
